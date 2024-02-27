1aa2f854075d7170233149daebecff430f098692
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-27 11:44:43 +0530

    BackGround Update

--- client/src/components/InfiniteMovingCardsDemo.jsx
+++ client/src/components/InfiniteMovingCardsDemo.jsx
@@ -5,7 +5,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export function InfiniteMovingCardsDemo() {
     return (
-        <div className="h-[35rem] flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <div className="h-[35rem] flex flex-col antialiased bg-black dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
             <InfiniteMovingCards
                 items={testimonials}
                 direction="right"
